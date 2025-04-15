4b7650484c4cc5e5d17923f3cc108e1b4a021f43
Musa Cekcen <cekcen14@gmail.com>
2025-04-15 15:14:49 +0200

    just started

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,22 +1,26 @@
 import express from "express";
 import {
- createUser,
- loginUser,
- logoutUser,
+  createUser,
+  loginUser,
+  logoutUser,
   getAllUsers,
   getUserById,
   updateUser,
-  deleteUser
+  deleteUser,
 } from "../controllers/userController.js";
-import { validateLogin,validateSignUp,validateUpdateUser } from "../middlewares/joiValidation.js";
+import {
+  validateLogin,
+  validateSignUp,
+  validateUpdateUser,
+} from "../middlewares/joiValidation.js";
 
 const router = express.Router();
-router.post(`/register`,validateSignUp,createUser);
-router.post(`/login`,validateLogin,loginUser);
+router.post(`/register`, validateSignUp, createUser);
+router.post(`/login`, validateLogin, loginUser);
 router.post(`/logout`, logoutUser);
 router.get("/", getAllUsers);
 router.get("/:id", getUserById);
-router.put("/:id",validateUpdateUser,updateUser);
+router.put("/:id", validateUpdateUser, updateUser);
 router.delete("/:id", deleteUser);
 
 export default router;
